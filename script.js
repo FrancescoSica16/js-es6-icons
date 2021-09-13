e1bf9fc789a02e1618c2a8b97e6a09cda028f4fa
FrancescoSica16 <francesco.sica16@gmail.com>
2021-09-13 16:16:10 +0200

    prima milestone printToPage

---
+++ script.js
@@ -0,0 +1,124 @@
+let icons = [
+	{
+		name: 'cat',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'crow',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'dog',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'dove',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'dragon',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'horse',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'hippo',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'fish',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas'
+	},
+	{
+		name: 'carrot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas'
+	},
+	{
+		name: 'apple-alt',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas'
+	},
+	{
+		name: 'lemon',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas'
+	},
+	{
+		name: 'pepper-hot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas'
+	},
+	{
+		name: 'user-astronaut',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas'
+	},
+	{
+		name: 'user-graduate',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas'
+	},
+	{
+		name: 'user-ninja',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas'
+	},
+	{
+		name: 'user-secret',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas'
+	}
+];
+
+const container = document.getElementsByClassName("my_icons-box");
+
+printToPage(icons , container);
+
+
+/**
+ * Mostro in pagina all'interno del container, in HTML, tutti gli elemetni dell'array dato
+ * @param array L'array da stampare
+ * @param container Il container (HTML Object) all'interno del quale inserire gli elementi
+ */
+ function printToPage(array, container) {
+
+    array.forEach((element) => {
+
+        const {name, prefix, family, type} = element;
+        
+        //aggiungo all'html del container il nostro contenueto in html
+        container.innerHTML += 
+        `<div class="col">
+        <i class="${family} ${prefix}${name}"> </i>
+        <h4 class=""my_icon-title>${name}</h4>
+        </div> 
+        `
+    });
+}
